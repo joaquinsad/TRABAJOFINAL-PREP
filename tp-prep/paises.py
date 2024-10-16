d5ed4606653f21a0c1efd70a8928d21be563d880
joaquinsad <joaquinchehin9@gmail.com>
2024-10-16 10:55:57 -0300

    agregamos los import a la pagina

--- tp-prep/paises.py
+++ tp-prep/paises.py
@@ -1,4 +1,14 @@
-@bp.route('/')
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for
+)
+from werkzeug.exceptions import abort
+
+from flaskr.auth import login_required
+from flaskr.db import get_db
+
+bp = Blueprint('blog', __name__)
+
+@bp.route('/paises')
 def index():
     db = get_db()
     posts = db.execute(
